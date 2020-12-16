f40e95a715ecd101fa9e2dc5dfdc5178b64f5f41
ivettemalagon <ivettemayerly@hotmail.com>
2020-12-16 05:45:33 -0500

    Updated CORS

--- src/components/UserBalance.vue
+++ src/components/UserBalance.vue
@@ -20,7 +20,7 @@ export default {
         this.username = this.$route.params.username
         let self = this
 
-        axios.get("https://cajero-app-ivette.herokuapp.com/user/balance/" + this.username)
+        axios.get("https://cajero-api-ivette.herokuapp.com/user/balance/" + this.username)
             .then((result) => {
                 self.balance = result.data.balance
             })
